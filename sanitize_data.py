d902cae8c469167c679a32424048d2008bf9ccb6
Wilson Qin <wilson.qin@gmail.com>
2014-12-01 23:16:57 -0500

    added comments to script

--- sanitize_data.py
+++ sanitize_data.py
@@ -4,11 +4,24 @@ fr_file = "spanish-test.txt"
 en_file = "english-test.txt"
 
 def tokenize(content):
+  """
+  Given a string, converts it into an array of alphanumeric tokens (i.e. words).
+
+  Args:
+    content: input string
+  Returns:
+    array of tokens (words)
+  Raises:
+    None
+  """
   regex = re.compile(r"[0-9a-z-'A-Z]+")
   token_list = regex.findall(content)
   return [w.strip().lower() for w in token_list]
 
 def main():
+  """
+    sanitizes text
+  """
   with open(fr_file) as f:
     fr_corpus = [tokenize(line) for line in f]
   with open(en_file) as f:
